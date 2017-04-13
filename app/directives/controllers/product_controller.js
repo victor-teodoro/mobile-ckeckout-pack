8b07abcbfcc727ea5621a7c74d69e343b69af349
Victor Teodoro <victor.teodoro@pagar.me>
2017-04-13 01:10:14 -0300

    Adiciona imagens dinâmicas

--- app/directives/controllers/product_controller.js
+++ app/directives/controllers/product_controller.js
@@ -1,5 +1,29 @@
-app.controller('productCtrl', ['$scope', function($scope) {
+app.controller('productCtrl', ['$scope', '$cookies', function($scope, $cookies) {
     // Infos do produto
     $scope.productId = 2345;
-    $scope.price = 239.90;
+    $scope.productName = $cookies.get('productName') || "Tênis Bela Veludo"
+    $scope.price = $cookies.get('price') || 239.90;
+    $scope.imgURL = $cookies.get('imgURL') || 'https://secure-static.arezzo.com.br/medias/sys_master/arezzo/arezzo/h31/hd0/h00/h00/8917607710750.jpg';
+    $scope.priceTemp = $cookies.get('priceTemp');
+    $scope.imgURLTemp = $cookies.get('imgURLTemp');
+    $scope.productNameTemp = $cookies.get('productNameTemp');
+
+    // Salva as infos do produto
+    $scope.saveProductInfo = function() {
+	// Preço
+	var priceTempNormalized = Number($scope.priceTemp.replace(',', '.'));
+	$scope.price = priceTempNormalized.toFixed(2);
+	$cookies.put('price', $scope.price);
+	$cookies.put('priceTemp', $scope.priceTemp);
+
+	// URL da imagem do produto
+	$scope.imgURL = $scope.imgURLTemp;
+	$cookies.put('imgURL', $scope.imgURL);
+	$cookies.put('imgURLTemp', $scope.imgURLTemp);
+
+	// O nome do produto a ser exibido
+	$scope.productName = $scope.productNameTemp;
+	$cookies.put('productName', $scope.productName);
+	$cookies.put('productNameTemp', $scope.productNameTemp);
+    };
 }]);
